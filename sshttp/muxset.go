2f55ead7cd8cb46f26e832c8df911f650d41267c
Emir Ozer <emirozer@yandex.com>
2015-08-08 23:56:31 +0200

    overhaul for multi SSH osqueryi dispatching

--- sshttp/muxset.go
+++ sshttp/muxset.go
@@ -3,9 +3,7 @@ package sshttp
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
-	"os/exec"
 	"strings"
 
 	"github.com/emirozer/exposq/osquery"
@@ -60,7 +58,7 @@ func SetMux(mux http.ServeMux) {
 	mux.HandleFunc("/kernel_info", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["kernel_info"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 
 		fmt.Fprintf(w, sout)
 
@@ -69,7 +67,7 @@ func SetMux(mux http.ServeMux) {
 	mux.HandleFunc("/kernel_integrity", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["kernel_integrity"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 
 		fmt.Fprintf(w, sout)
 	})
@@ -77,328 +75,309 @@ func SetMux(mux http.ServeMux) {
 	mux.HandleFunc("/kernel_modules", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["kernel_modules"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/mounts", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["mounts"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/mounts_device_alias_none", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["mounts_device_alias_none"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/block_devices", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["block_devices"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/block_device_sz0", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["block_device_sz0"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/block_device_ata", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["block_device_ata"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/acpi_tables", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["acpi_tables"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/cpuid", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["cpuid"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/crontab", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["crontab"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/disk_encryption", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["disk_encryption"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/etc_hosts", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["etc_hosts"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/etc_protocols", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["etc_protocols"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/etc_services", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["etc_services"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/groups", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["groups"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/interface_addresses", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["interface_addresses"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/interface_details", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["interface_details"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/iptables", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["iptables"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/last", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["last"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/listening_ports", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["listening_ports"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/logged_in_users", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["logged_in_users"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/memory_map", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["memory_map"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/pci_devices", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["pci_devices"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/passwd_changes", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["passwd_changes"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/processes", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["processes"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/processes_root", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["processes_root"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/process_envs", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["process_envs"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/process_memory_map", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["process_memory_map"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/process_open_files", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["process_open_files"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/process_open_sockets", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["process_open_sockets"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/routes", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["routes"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/shared_memory", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["shared_memory"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/shell_history", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["shell_history"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/users", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["users"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/user_groups", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["user_groups"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/uptime", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["uptime"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/relay", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["relay"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/mitm", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["mitm"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/setuid_enabled", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["setuid_enabled"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/q_scan_ps_bin", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["q_scan_ps_bin"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/ps_lst_tcp_udp", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + oq["ps_lst_tcp_udp"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/rpm_package_files", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + coq["rpm_package_files"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/rpm_packages", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + coq["rpm_packages"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/apt_sources", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + doq["apt_sources"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
 	mux.HandleFunc("/deb_packages", func(w http.ResponseWriter, req *http.Request) {
 
 		cmd := "osqueryi " + "\"" + doq["deb_packages"] + "\""
-		sout := dispatchCmd(cmd)
+		sout := MainSshHandler(cmd)
 		fmt.Fprintf(w, sout)
 	})
 
-}
-
-func dispatchCmd(cmd string) string {
-
-	out, err := exec.Command("sh", "-c", cmd).Output()
-	if err != nil {
-		log.Println(err)
-	}
-	sout := string(out[:])
-
-	if len(sout) == 0 && (strings.Contains(cmd, "apt_resources") || strings.Contains(cmd, "deb_packages")) {
-		sout = fmt.Sprintf("Target is RPM based, query won't return anything: %v", cmd)
-	} else if len(sout) == 0 && (strings.Contains(cmd, "rpm_package_files") || strings.Contains(cmd, "rpm_packages")) {
-		sout = fmt.Sprintf("Target is APT based, query won't return anything: %v", cmd)
-	} else if len(sout) == 0 {
-		sout = fmt.Sprintf("No response for the following query from this machine : %v", cmd)
-	}
-
-	return sout
 }
--- sshttp/ssh.go
+++ sshttp/ssh.go
@@ -1,48 +1,107 @@
 package sshttp
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"os"
 	"strings"
 
-	"github.com/hypersleep/easyssh"
+	json "github.com/bitly/go-simplejson"
+	"golang.org/x/crypto/ssh"
 )
 
-//Reads the target file, returns a slice of string slices containing user/ip
-//E.g. :[[ec2-user 52.18.229.188] [centos  52.17.136.207]]
-func readTarget() {
+func readTarget() ([][]string, string) {
 
 	target_list := make([][]string, 0)
-	t, err := ioutil.ReadFile("../target")
+	t, err := os.Open("target.json")
+	defer t.Close()
 	if err != nil {
 		panic(err)
 	}
-	at := string(t)
-	te := strings.Fields(at)
-	for _, i := range te {
 
-		target_list = append(target_list, strings.Split(i, "@"))
+	js, err := json.NewFromReader(t)
+	if err != nil {
+		log.Fatal(err)
+	}
+	key, _ := js.Get("key").String()
 
+	targets := js.Get("targets")
+	temp_t, _ := js.Get("targets").Array()
+
+	for k, _ := range temp_t {
+		temp_slice := make([]string, 0)
+		user, _ := targets.GetIndex(k).Get("user").String()
+		ip, _ := targets.GetIndex(k).Get("ip").String()
+
+		temp_slice = append(temp_slice, user)
+		temp_slice = append(temp_slice, ip)
+		target_list = append(target_list, temp_slice)
 	}
 
+	return target_list, key
 }
 
-func sshStuff() {
-	ssh := &easyssh.MakeConfig{
-		User:   "john",
-		Server: "example.com",
-		// Optional key or Password without either we try to contact your agent SOCKET
-		Key:  "~/.ssh/id_rsa",
-		Port: "22",
-	}
-
-	// Call Run method with command you want to run on remote server.
-	response, err := ssh.Run("ps ax")
-	// Handle errors
+func sshDispatch(cmd string, user string, ip string, key string) string {
+	var res string
+	pemBytes, err := ioutil.ReadFile(key)
 	if err != nil {
-		panic("Can't run remote command: " + err.Error())
-	} else {
-		fmt.Println(response)
+		log.Fatal(err)
+	}
+	signer, err := ssh.ParsePrivateKey(pemBytes)
+	if err != nil {
+		log.Fatalf("parse key failed:%v", err)
+	}
+	config := &ssh.ClientConfig{
+		User: user,
+		Auth: []ssh.AuthMethod{ssh.PublicKeys(signer)},
+	}
+	conn, err := ssh.Dial("tcp", ip+":22", config)
+	if err != nil {
+		log.Fatalf("dial failed:%v", err)
+	}
+	defer conn.Close()
+	session, err := conn.NewSession()
+	if err != nil {
+		log.Fatalf("session failed:%v", err)
+	}
+	defer session.Close()
+	var stdoutBuf bytes.Buffer
+	session.Stdout = &stdoutBuf
+	err = session.Run(cmd)
+	if err != nil {
+		log.Fatalf("Run failed:%v", err)
 	}
 
+	res = stdoutBuf.String()
+	return res
+}
+
+func MainSshHandler(cmd string) string {
+	var target_list [][]string
+	var key string
+	var user string
+	var ip string
+	var sout string
+	target_list, key = readTarget()
+
+	for _, j := range target_list {
+
+		user = j[0]
+		ip = j[1]
+
+		out := sshDispatch(cmd, user, ip, key)
+		sout += string(out[:])
+	}
+
+	if len(sout) == 0 && (strings.Contains(cmd, "apt_resources") || strings.Contains(cmd, "deb_packages")) {
+		sout = fmt.Sprintf("Target is RPM based, query won't return anything: %v", cmd)
+	} else if len(sout) == 0 && (strings.Contains(cmd, "rpm_package_files") || strings.Contains(cmd, "rpm_packages")) {
+		sout = fmt.Sprintf("Target is APT based, query won't return anything: %v", cmd)
+	} else if len(sout) == 0 {
+		sout = fmt.Sprintf("No response for the following query from this machine : %v", cmd)
+	}
+
+	return sout
 }
